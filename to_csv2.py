959882463b7e1ac584608e7d797dd143521440cd
RA Stephen <alick1580@gmail.com>
2019-11-22 16:28:17 -0500

    tidying up the code

--- to_csv2.py
+++ to_csv2.py
@@ -1,27 +1,10 @@
 import pandas as pd
 import numpy as np
-from columns import *
-from colspecs import *
+from columns import * # Column Headers
+from colspecs import * # Column length specs
 
-# colspecs = [(8,12),(12,14),(18,22),(22,23),(31,32),(32,33),(49,50),
-#     (72,73),(73,74),(74,76),
-#     (76,78),(78,79),(83,84),(103,104)
-#     ]
+df = pd.read_fwf('Nat2018Head.txt', colspecs=list_colspecs, header=None, )
 
-# columns = ["DOB_YY", "DOB_MM", "DOB_TT","DOB_WK","BFACIL","F_FACILITY","BFACIL3",
-#     "MAGE_IMPFLG","MAGE_REPFLG","MAGER",
-#     "MAGER14","MAGER9","MBSTATE_REC","RESTATUS"
-#     ]
-
-df = pd.read_fwf('Nat2018Head copy.txt', colspecs=colspecs, header=None, )
-
-
-# df1 = pd.DataFrame(df.values, columns = columns)
 df_columns
-df.columns = df_columns
+df.columns = list_columns
 df
-# print(open('Nat2018Head.txt').read())
-df_columns
-colspecs
-
-
