d2fda5453663c4b3e470354ef6c9329feb2cd807
RA Stephen <alick1580@gmail.com>
2019-12-03 18:27:46 -0500

    commt

--- ml/modeling.py
+++ ml/modeling.py
@@ -21,16 +21,16 @@ def label_encoding(df):
 
 
 # Random Forests
-def random_forest(x, y, test_size=0.33, random_state=0, **params):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size, random_state)
+def random_forest(x, y, test_size_val=0.33, random_state_val=0, **params):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = test_size_val, random_state = random_state_val)
     model = RandomForestClassifier(n_estimators=100, oob_score=True, **params)  # max_features=5
     model.fit(x_train, y_train)
     return model, x_train, x_test, y_train, y_test
 
 
 # Support Vector Machine
-def support_vector_machine(x, y, test_size=0.33, random_state=0):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size, random_state, **params)
+def support_vector_machine(x, y, test_size_val=0.33, random_state_val=0):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = test_size_val, random_state = random_state_val, **params)
     model = SVC()
     model.fit(x_train, y_train)
     return model, x_train, x_test, y_train, y_test
@@ -43,8 +43,8 @@ def confusion_matrix(model, x_test, y_test):
 
 
 # xgb_classifier
-def xgb_classifier(x, y, test_size=0.33, random_state=0, **params):
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size, random_state)
+def xgb_classifier(x, y, test_size_val=0.33, random_state_val=0, **params):
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = test_size_val, random_state = random_state_val)
     model = XGBClassifier(**params)
     model.fit(x_train, y_train)
     return model, x_train, x_test, y_train, y_test
