1edbd8c116433d4dc14b4f6c48ed7613f8ef80d7
RA Stephen <alick1580@gmail.com>
2019-12-04 18:18:29 -0500

    tabel in .env

--- ml/data_load.py
+++ ml/data_load.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 import os
 
-
 def fetchCollumns(columns_list, extra_query=''):
     engine = create_engine("mysql+pymysql://{user}:{pw}@localhost/{db}"
                            .format(user=os.getenv("DB_USER"),
@@ -13,9 +12,13 @@ def fetchCollumns(columns_list, extra_query=''):
                                    db=os.getenv("DB_DATABASE")))
 
     cols = ', '.join(columns_list)
+    if type(os.getenv("DB_TABLE")) != 'NoneType' :
+        database_table = os.getenv("DB_TABLE") 
+    else:
+        database_table = "all_years_proper_template"
 
     # %time df = pd.read_sql_query('SELECT DOB_YY,AB_AVEN1,AB_AVEN6,AB_SURF,AB_ANTI,AB_SEIZ,F_AB_VENT,F_AB_VENT6,F_AB_NIUC,F_AB_SURFAC,F_AB_ANTIBIO,F_AB_SEIZ,NO_ABNORM,AB_NICU FROM all_years_data;', engine)
-    df = pd.read_sql_query('SELECT ' + cols + ' FROM all_years_proper_template' + extra_query, engine)
+    df = pd.read_sql_query('SELECT ' + cols + ' FROM '+ database_table +' '+ extra_query, engine)
 
     return df
 
