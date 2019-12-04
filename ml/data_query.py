77def9b9b29ba15cab8e59a9acaf42823bf581b6
bariki <bariki.elilaki@gmail.com>
2019-12-04 11:33:33 -0500

    data set loader 1

---
+++ ml/data_query.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+from sklearn import datasets #sample dataset
+import ml.modeling as modeling
+from importlib import reload
+reload(modeling) #to reload module if we made some changes
+
+temp1 = datasets.load_breast_cancer() #dataset as temp1
+x = temp1.data[:, :3]  # we only take the first 3 features.
+y = temp1.target
+
+model, x_train, x_test, y_train, y_test = modeling.random_forest(x,y) #create model and fit
+
+model.score(x_test,y_test) #check model score
+modeling.get_confusion_matrix(model,x_test,y_test) #print confusion matrix
+
+
