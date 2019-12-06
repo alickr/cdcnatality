78c7e54d7eeb1e7ee358b67ef9c9956d229fb47d
RA Stephen <alick1580@gmail.com>
2019-12-06 15:30:51 -0500

    data query

--- ml/data_query.py
+++ ml/data_query.py
@@ -18,7 +18,7 @@ cols2 = ['DOB_YY', 'AB_AVEN1', 'AB_AVEN6', 'AB_SURF', 'AB_ANTI', 'AB_SEIZ', 'F_A
          'F_AB_NIUC', 'F_AB_SURFAC', 'F_AB_ANTIBIO', 'F_AB_SEIZ', 'NO_ABNORM', 'AB_NICU']
 
 selected_columns
-df = data_load.fetchCollumns(selected_columns, extra_query='LIMIT 1000') #extra_query='LIMIT 100'
+%time df = data_load.fetchCollumns(selected_columns, extra_query='LIMIT 1000 ') #extra_query='LIMIT 100'
 # df.isnull().sum()
 df = df[df.AB_NICU != "U"] #Drop Column with U Unknown 
 df = df[df.AB_NICU != " "] #Drop Empty Space Columsn Not Treated as Nan
@@ -29,28 +29,30 @@ x = df.loc[:, ~df.columns.isin(['AB_NICU', 'DOB_YY'])]  # Remove Specific column
 y = df.AB_NICU.values
 
 # Random Forests
-random_forest_model, x_train1, x_test1, y_train1, y_test1 = modeling.random_forest(x, y)  # create model and fit
+%time random_forest_model, x_train1, x_test1, y_train1, y_test1 = modeling.random_forest(x, y,class_weight="balanced",)  # create model and fit
 random_forest_model.score(x_test1, y_test1)  # check model score
 modeling.get_confusion_matrix(random_forest_model, x_test1, y_test1)  # print confusion matrix
 modeling.get_feature_importance(x,random_forest_model) # Plot Feature Importance
+
+#XGB
+%time xgb_model, x_train3, x_test3, y_train3, y_test3 = modeling.xgb_classifier(x, y)  # create model and fit
+xgb_model.score(x_test3, y_test3)  # check model score
+modeling.get_confusion_matrix(xgb_model, x_test3, y_test3)  # print confusion matrix
+modeling.get_feature_importance(x,xgb_model) # Plot Feature Importance
 
 #SVM
 svm_model, x_train2, x_test2, y_train2, y_test2 = modeling.support_vector_machine(x, y)  # create model and fit
 svm_model.score(x_test2, y_test2)  # check model score
 modeling.get_confusion_matrix(svm_model, x_test2, y_test2)  # print confusion matrix
 
-#XGB
-xgb_model, x_train3, x_test3, y_train3, y_test3 = modeling.xgb_classifier(x, y)  # create model and fit
-xgb_model.score(x_test3, y_test3)  # check model score
-modeling.get_confusion_matrix(xgb_model, x_test3, y_test3)  # print confusion matrix
-modeling.get_feature_importance(x,xgb_model) # Plot Feature Importance
-
-
-# df.AB_NICU.unique()
-# df.AB_NICU.isna().sum()
-# print (df[df.AB_NICU == "U"].shape[0])
+df.AB_NICU.unique()
+df.AB_NICU.isna().sum()
+print (df[df.AB_NICU == "Y"].shape[0])
+random_forest_model.x_train
 # df = df[df.AB_NICU != "U"]
 # df = df[df.AB_NICU != " "]
-# df.shape
+df.corr()
+
+df.shape
 
 
