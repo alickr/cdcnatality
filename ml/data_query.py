8c32a043e7bbc2bce40acc60fba4fe7d8285d2b5
RA Stephen <alick1580@gmail.com>
2019-12-06 19:49:06 -0500

    commit

--- ml/data_query.py
+++ ml/data_query.py
@@ -30,30 +30,35 @@ x = df.loc[:, ~df.columns.isin(['AB_NICU', 'DOB_YY'])]  # Remove Specific column
 y = df.AB_NICU.values
 
 # Random Forests
-%time random_forest_model, x_train1, x_test1, y_train1, y_test1 = modeling.random_forest(x, y,class_weight="balanced",)  # create model and fit
+%time random_forest_model, x_train1, x_test1, y_train1, y_test1 = modeling.random_forest(x, y,class_weight={0:1,1:10},)  # create model and fit # class_weight="balanced"
 random_forest_model.score(x_test1, y_test1)  # check model score
 modeling.get_confusion_matrix(random_forest_model, x_test1, y_test1)  # print confusion matrix
+modeling.get_normalized_confusion_matrix(random_forest_model, x_test1, y_test1)  # print Normalized confusion matrix
 modeling.get_feature_importance(x,random_forest_model) # Plot Feature Importance
 
 #XGB
 %time xgb_model, x_train3, x_test3, y_train3, y_test3 = modeling.xgb_classifier(x, y)  # create model and fit
 xgb_model.score(x_test3, y_test3)  # check model score
 modeling.get_confusion_matrix(xgb_model, x_test3, y_test3)  # print confusion matrix
+modeling.get_normalized_confusion_matrix(xgb_model, x_test3, y_test3)  # print Normalized confusion matrix
+
 modeling.get_feature_importance(x,xgb_model) # Plot Feature Importance
 
 #SVM
 svm_model, x_train2, x_test2, y_train2, y_test2 = modeling.support_vector_machine(x, y)  # create model and fit
 svm_model.score(x_test2, y_test2)  # check model score
 modeling.get_confusion_matrix(svm_model, x_test2, y_test2)  # print confusion matrix
+modeling.get_normalized_confusion_matrix(svm_model, x_test2, y_test2)  # print Normalized confusion matrix
 
-df.AB_NICU.unique()
-df.AB_NICU.isna().sum()
-print (df[df.AB_NICU == "Y"].shape[0])
-random_forest_model.x_train
-# df = df[df.AB_NICU != "U"]
-# df = df[df.AB_NICU != " "]
-df.corr()
+# ---- Code For Testing Below Will Be deleted Finally ---- #
+# df.AB_NICU.unique()
+# df.AB_NICU.isna().sum()
+# print (df[df.AB_NICU == "Y"].shape[0])
+# random_forest_model.x_train
+# # df = df[df.AB_NICU != "U"]
+# # df = df[df.AB_NICU != " "]
+# df.corr()
 
-df.shape
-
+# df.shape
+# type(df)
 
